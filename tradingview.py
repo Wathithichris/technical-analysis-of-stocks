9234c099318d9c926b43d4737d09f13fbcc7084b
Chris <mwangichris675@gmail.com>
2024-12-14 15:16:10 +0300

    Get KCB csv data

--- tradingview.py
+++ tradingview.py
@@ -8,15 +8,13 @@ username = 'YourTradingViewUsername'
 password = 'YourTradingViewPassword'
 
 tv = TvDatafeed(username, password)
-symbols = ["KCB"]
+symbol = "KCB"
 
 def get_data(symbol):
     data = tv.get_hist(symbol=symbol, exchange='NSEKE', interval=Interval.in_daily, n_bars=8000)
     csv_data = data.to_csv(f"{symbol}.csv")
     return csv_data
 
-if __name__ =="--main__":
-    for symbol in symbols:
-        get_data(symbol)
-        time.sleep(1)
+get_data(symbol)
+
 
